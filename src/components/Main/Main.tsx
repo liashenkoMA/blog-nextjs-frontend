943fabb92e6dc5daa702582e4fea2ae5f1b9cf4e
LiashenkoMA <makcim_an@mail.ru>
2023-12-15 00:34:52 +0400

    feat: компонент contact

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -2,6 +2,7 @@ import "./main.scss";
 import Developer from "../Developer/Developer";
 import AboutMe from "../AboutMe/AboutMe";
 import Skills from "../Skills/Skills";
+import Contatc from "../Contact/Contact";
 
 export default function Main() {
   return (
@@ -9,6 +10,7 @@ export default function Main() {
       <Developer />
       <AboutMe />
       <Skills />
+      <Contatc />
     </main>
   );
 }
