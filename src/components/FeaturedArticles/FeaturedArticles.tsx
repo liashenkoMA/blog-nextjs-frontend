f310e4bbe3b171e6f804965ff3fc55ea2a01d6d2
LiashenkoMA <makcim_an@mail.ru>
2023-12-21 00:13:25 +0400

    feat: верстка избранных статей

--- src/components/FeaturedArticles/FeaturedArticles.tsx
+++ src/components/FeaturedArticles/FeaturedArticles.tsx
@@ -1,9 +1,33 @@
 import "./featuredarticles.scss";
+import Image from "next/image";
+import Link from "next/link";
+
+import arrow from "../../images/arrow-right.png";
+import FeaturedArticlesCard from "../FeaturedArticlesCard/FeaturedArticlesCard";
 
 export default function FeaturedArticles() {
   return (
     <section className="featuredarticles">
-
+      <div className="featuredarticles__conteiner">
+        <h2 className="featuredarticles__title">Featured Articles</h2>
+        <p className="featuredarticles__description">
+          Discover the most outstanding articles in all topics
+        </p>
+        <div className="featuredarticles__cards">
+          <FeaturedArticlesCard />
+          <FeaturedArticlesCard />
+          <FeaturedArticlesCard />
+          <FeaturedArticlesCard />
+          <FeaturedArticlesCard />
+          <FeaturedArticlesCard />
+          <FeaturedArticlesCard />
+          <FeaturedArticlesCard />
+        </div>
+        <Link href="#" className="featuredarticles__link">
+          Show More Posts
+          <Image src={arrow} width={14} height={14} alt="Arrow" className="featuredarticles__icon" />
+        </Link>
+      </div>
     </section>
-  )
+  );
 }
---
+++ src/components/FeaturedArticlesCard/FeaturedArticlesCard.tsx
@@ -0,0 +1,65 @@
+import "./featuredarticlescard.scss";
+import Image from "next/image";
+import Link from "next/link";
+
+import time from "../../images/Time.png";
+import im from "../../images/photo.jpg";
+
+export default function FeaturedArticlesCard() {
+  return (
+    <div className="featuredarticlescard">
+      <div className="featuredarticlescard__conteiner">
+        <Link href="#" className="featuredarticlescard__images-link">
+          <Image
+            src={im}
+            width={305}
+            height={200}
+            alt="#"
+            loading="lazy"
+            className="featuredarticlescard__images"
+          />
+        </Link>
+        <div className="featuredarticlescard__informations">
+          <Link href="#" className="featuredarticlescard__info link_color">
+            #Design
+          </Link>
+          <p className="featuredarticlescard__info">
+            <Image
+              src={time}
+              width={18}
+              height={18}
+              alt="Time"
+              className="featuredarticlescard__timeread-icon"
+            />
+            89 mins read
+          </p>
+        </div>
+        <Link href="#" className="featuredarticlescard__title link_color">
+          Facts About Business That Will Help You Success
+        </Link>
+        <div className="featuredarticlescard__nav">
+          <div className="featuredarticlescard__author">
+            <Image
+              src={im}
+              width={48}
+              height={48}
+              loading="lazy"
+              alt="Author photo"
+              className="featuredarticlescard__author-photo"
+            />
+            <div className="featuredarticlescard__author-info">
+              <p className="featuredarticlescard__author-name">Makcim</p>
+              <p className="featuredarticlescard__date-publick">27 june 2023</p>
+            </div>
+          </div>
+          <Link href="#" className="featuredarticlescard__nav-link">
+            <div className="featuredarticlescard__round-pic"></div>
+            <p className="featuredarticlescard__nav-link_type_text">
+              Read more
+            </p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
